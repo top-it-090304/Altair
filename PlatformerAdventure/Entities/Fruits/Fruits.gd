8d8bf1e6c25c8e3b54715dbba73faa9bd8b6b220
vladislav <vladellert3438@mail.ru>
2026-02-24 21:22:54 +0600

    Added: transition between levels, and level completion logic with a flag node

--- PlatformerAdventure/Entities/Fruits/Fruits.gd
+++ PlatformerAdventure/Entities/Fruits/Fruits.gd
@@ -3,6 +3,8 @@ extends Area2D
 @onready var collision_shape = $CollisionShape2D  
 @onready var animated_sprite = $AnimatedSprite2D  
 
+signal collected
+
 func _ready():  
 	animated_sprite.play("fruits")  
 	
@@ -13,4 +15,5 @@ func _on_body_entered(body: Node2D):
 		collision_shape.set_deferred("disabled", true)  
 		animated_sprite.play("collected")  
 		await get_tree().create_timer(0.5).timeout  
+		collected.emit()
 		queue_free()  
