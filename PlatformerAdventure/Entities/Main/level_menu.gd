eb9be41641a46671355646eadc9f8a1ae1043232
vladislav <vladellert3438@mail.ru>
2026-02-26 21:08:32 +0600

    added Level1 for pinkman

--- PlatformerAdventure/Entities/Main/level_menu.gd
+++ PlatformerAdventure/Entities/Main/level_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Levels/PinkMan_1-12Levels/LevelX.tscn")
+	get_tree().change_scene_to_file("res://Levels/PinkMan_1-12Levels/Level1.tscn")
 
 
 func _on_button_2_pressed() -> void:
