97cb3fbb619562382735392ebb2ed1a15f8d78f8
vladislav <vladellert3438@mail.ru>
2026-02-27 11:48:22 +0600

    updated level menu and level4

--- PlatformerAdventure/Entities/Main/level_menu.gd
+++ PlatformerAdventure/Entities/Main/level_menu.gd
@@ -1,24 +1,30 @@
 extends Control
 
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Entities/Main/MainMenu.tscn") # Выход в мейн
+	
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Levels/PinkMan_1-12Levels/Level1.tscn")
+	get_tree().change_scene_to_file("res://Levels/PinkMan_1-12Levels/Level1.tscn") # Просто пропрыгать
 
 
 func _on_button_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://Levels/PinkMan_1-12Levels/Level2.tscn")
+	get_tree().change_scene_to_file("res://Levels/PinkMan_1-12Levels/LevelG.tscn") # Платформа
 
 
 func _on_button_3_pressed() -> void:
-	get_tree().change_scene_to_file("res://Levels/Level3/Level3.tscn")
-
+	get_tree().change_scene_to_file("res://Levels/PinkMan_1-12Levels/Level5.tscn") # Кубы
 
 func _on_button_4_pressed() -> void:
-	get_tree().change_scene_to_file("res://Levels/Level4/Level4.tscn")
-
-
-func _on_texture_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Entities/Main/MainMenu.tscn")
+	get_tree().change_scene_to_file("res://Levels/Level3/Level3.tscn") # Тот который андрей делает с огнем
 
 
 func _on_button_5_pressed() -> void:
-	get_tree().change_scene_to_file("res://Levels/PinkMan_1-12Levels/Level5.tscn")
+	get_tree().change_scene_to_file("res://Levels/NinjaFrog_37-50Levels/LevelN.tscn") #Ниндзяфрог цепи
+
+
+func _on_button_6_pressed() -> void:
+	get_tree().change_scene_to_file("res://Levels/VirtualGuy_13-24Levels/LevelV.tscn") # ВиартуалГай шипы 
+
+
+func _on_button_7_pressed() -> void:
+	get_tree().change_scene_to_file("res://Levels/PinkMan_1-12Levels/Level2.tscn") # Пинкмен шестерни
