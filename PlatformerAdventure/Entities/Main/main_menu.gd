d8a5b6323b8f7a26973a1a0b5f66e18c8b906a45
vladislav <vladellert3438@mail.ru>
2026-03-26 22:07:58 +0600

    unsuccessful attempt to fix a bug with a button

--- PlatformerAdventure/Entities/Main/main_menu.gd
+++ PlatformerAdventure/Entities/Main/main_menu.gd
@@ -1,9 +1,8 @@
 extends Control
 
-func _on_texture_button_pressed() -> void:
+func _on_texture_button_button_down() -> void:
 	get_tree().change_scene_to_file("res://Entities/Main/Levels_Menu.tscn")
-	
 
 
-func _on_texture_button_2_pressed() -> void:
+func _on_texture_button_2_button_down() -> void:
 	get_tree().change_scene_to_file("res://Entities/Shop/Shop.tscn")
