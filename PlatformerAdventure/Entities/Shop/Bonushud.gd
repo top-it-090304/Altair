c96a420a456d719a1bcb70bc812eb662e2376485
Vladislav Ellert <vladellert3438@mail.ru>
2026-03-30 16:06:55 +0600

    fixed Level1 and Lelev3: I did this levels more easier

--- PlatformerAdventure/Entities/Shop/Bonushud.gd
+++ PlatformerAdventure/Entities/Shop/Bonushud.gd
@@ -4,48 +4,16 @@ extends CanvasLayer
 @onready var btn_slowmo: TextureButton = $PanelContainer/BonusList/BtnSlowmo
 @onready var btn_magnet: TextureButton = $PanelContainer/BonusList/BtnMagnet
 @onready var bonus_list: VBoxContainer = $PanelContainer/BonusList
-@onready var toggle_btn: TextureButton = $PanelContainer/ToggleBtn
 
-const ANIM_DURATION := 0.2
-
-var _is_open := false
 var _tween: Tween
 
 func _ready() -> void:
 	btn_shield.pressed.connect(_on_use_shield)
 	btn_slowmo.pressed.connect(_on_use_slowmo)
 	btn_magnet.pressed.connect(_on_use_magnet)
-	toggle_btn.pressed.connect(_on_toggle)
-	
-	# Скрываем список сразу без анимации
-	bonus_list.visible = false
-	bonus_list.modulate.a = 0.0
 	
 	_refresh()
 
-func _on_toggle() -> void:
-	if _is_open:
-		_close()
-	else:
-		_open()
-
-func _open() -> void:
-	_is_open = true
-	bonus_list.visible = true
-	
-	if _tween:
-		_tween.kill()
-	_tween = create_tween()
-	_tween.tween_property(bonus_list, "modulate:a", 1.0, ANIM_DURATION)
-
-func _close() -> void:
-	_is_open = false
-	
-	if _tween:
-		_tween.kill()
-	_tween = create_tween()
-	_tween.tween_property(bonus_list, "modulate:a", 0.0, ANIM_DURATION)
-	_tween.tween_callback(func(): bonus_list.visible = false)
 
 func _refresh() -> void:
 	btn_shield.disabled = not GameData.purchased_shield
