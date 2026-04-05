df6b832b837ce4b5b7597c70e341ff7d9e8a270e
andreykolokhmatov <72th5c7k8y@privaterelay.appleid.com>
2026-04-05 13:12:48 +0600

    added  full level menu mask dude

--- PlatformerAdventure/Entities/Shop/GameData.gd
+++ PlatformerAdventure/Entities/Shop/GameData.gd
@@ -7,7 +7,9 @@ const SAVE_PATH = "user://save.cfg"
 
 const LEVEL_ORDER: Array = [
 	"Level1", "Level2", "Level3", "Level4", "Level5",
-	"Level6", "Level7", "Level8"
+	"Level6", "Level7", "Level8",
+	"Level9", "Level10", "Level11", "Level12", "Level13",
+	"Level14", "Level15", "Level16"
 ]
 
 var level_records: Dictionary = {}
