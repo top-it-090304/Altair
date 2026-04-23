0e89aaa98251668b91bafd4e83d65725d3ccd16a
andrey kolokhmatov <72th5c7k8y@privaterelay.appleid.com>
2026-04-23 16:07:49 +0600

    add about popup with pixel font

--- PlatformerAdventure/Entities/Main/about_popup.gd
+++ PlatformerAdventure/Entities/Main/about_popup.gd
@@ -1,5 +1,19 @@
 extends CanvasLayer
 
+func _ready() -> void:
+	hide()
+	var font = preload("res://Assets/Fonts/EpilepsySansBold.ttf")
+	var font_bold = preload("res://Assets/Fonts/EpilepsySansBold.ttf")
+
+	for label in _get_all_labels(self):
+		label.add_theme_font_override("font", font)
+		label.add_theme_font_size_override("font_size", 24)
+
+	var title = find_child("Title", true, false)
+	if title:
+		title.add_theme_font_override("font", font_bold)
+		title.add_theme_font_size_override("font_size", 36)
+
 func show_popup() -> void:
 	visible = true
 
@@ -8,3 +22,11 @@ func hide_popup() -> void:
 
 func _on_close_pressed() -> void:
 	hide_popup()
+
+func _get_all_labels(node: Node) -> Array:
+	var labels := []
+	for child in node.get_children():
+		if child is Label:
+			labels.append(child)
+		labels.append_array(_get_all_labels(child))
+	return labels
